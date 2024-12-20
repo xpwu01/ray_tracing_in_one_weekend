e63fc4cbb0c6f2fa07f6caa5248077251703a2bd
Xiaopeng Wu <xiaopengwu2001@gmail.com>
2024-12-20 19:51:13 +0000

    Week two final scene.

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # Ray Tracing in One Weekend
 
-This is a Rust implementation of Peter Shirley's "[_Ray Tracing in One Weekend_](https://raytracing.github.io/books/RayTracingInOneWeekend.html)" tutorial series for learning purpose.
+This is a Rust implementation of Peter Shirley's "[_Ray Tracing in One Weekend_](https://raytracing.github.io)" book series for learning purpose.
 
 ## Building and Running
 
-Ensure you have the Rust toolchain installed, then execute:
+Ensure you have the Rust toolchain installed, clone and `cd` into the project directory, then execute:
 
 `cargo run --release > image.ppm`
 
-You should see something akin to the below image after the render has finished.
+You should see something akin to the below image after the render has finished (took around 11hr30min on a M1 Pro).
 
-![Final Render](week_one.png)
+![Final Render](week_two.png)
 
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use ray_tracing_in_one_weekend::*;
 
 fn main() {
-    let case = 8;
+    let case = 9;
 
     match case {
+        0 => final_scene(400, 250, 4),
         1 => bouncing_spheres(),
         2 => checkered_spheres(),
         3 => earth(),
@@ -12,6 +13,7 @@ fn main() {
         6 => simple_light(),
         7 => cornell_box(),
         8 => cornell_smoke(),
+        9 => final_scene(800, 10000, 40),
         _ => panic!("Invalid case"),
     }
 }
@@ -565,6 +567,149 @@ fn cornell_smoke() {
 
     let vfov: f64 = 40.0;
     let look_from = Point3::new(278.0, 278.0, -800.0);
+    let look_at = Point3::new(278.0, 278.0, 0.0);
+    let vup = Vec3::new(0.0, 1.0, 0.0);
+
+    let defocus_angle = 0.0;
+    let focus_distance = 10.0;
+
+    let cam = Camera::new(
+        aspect_ratio,
+        image_width,
+        max_depth,
+        samples_per_pixel,
+        vfov,
+        look_from,
+        look_at,
+        vup,
+        defocus_angle,
+        focus_distance,
+        background,
+    );
+
+    cam.render(&world);
+}
+
+fn final_scene(image_width: u32, samples_per_pixel: u32, max_depth: u32) {
+    let mut boxes1 = HittableList::empty();
+
+    let ground = Rc::new(Lambertian::from_colour(Colour::new(0.48, 0.83, 0.53)));
+
+    let boxes_per_side = 20;
+
+    for i in 0..boxes_per_side {
+        for j in 0..boxes_per_side {
+            let w = 100.0;
+            let x0 = -1000.0 + i as f64 * w;
+            let z0 = -1000.0 + j as f64 * w;
+            let y0 = 0.0;
+            let x1 = x0 + w;
+            let y1 = random_double_range(1.0, 101.0);
+            let z1 = z0 + w;
+
+            boxes1.add(Quad::block(
+                &Point3::new(x0, y0, z0),
+                &Point3::new(x1, y1, z1),
+                ground.clone(),
+            ));
+        }
+    }
+
+    let mut world = HittableList::empty();
+
+    world.add(Rc::new(BVHNode::from_list(boxes1)));
+
+    let light = Rc::new(DiffuseLight::from_colour(Colour::new(7.0, 7.0, 7.0)));
+    world.add(Rc::new(Quad::new(
+        Point3::new(123.0, 554.0, 147.0),
+        Vec3::new(300.0, 0.0, 0.0),
+        Vec3::new(0.0, 0.0, 265.0),
+        light,
+    )));
+
+    let centre1 = Point3::new(400.0, 400.0, 200.0);
+    let centre2 = centre1 + Vec3::new(30.0, 0.0, 0.0);
+    let sphere_material = Rc::new(Lambertian::from_colour(Colour::new(0.7, 0.3, 0.1)));
+    world.add(Rc::new(Sphere::new(
+        centre1,
+        centre2,
+        50.0,
+        sphere_material,
+    )));
+
+    world.add(Rc::new(Sphere::new(
+        Point3::new(260.0, 150.0, 45.0),
+        Point3::new(260.0, 150.0, 45.0),
+        50.0,
+        Rc::new(Dielectric::new(1.5)),
+    )));
+    world.add(Rc::new(Sphere::new(
+        Point3::new(0.0, 150.0, 145.0),
+        Point3::new(0.0, 150.0, 145.0),
+        50.0,
+        Rc::new(Metal::new(Colour::new(0.8, 0.8, 0.9), 1.0)),
+    )));
+
+    let boundary = Rc::new(Sphere::new(
+        Point3::new(360.0, 150.0, 145.0),
+        Point3::new(360.0, 150.0, 145.0),
+        70.0,
+        Rc::new(Dielectric::new(1.5)),
+    ));
+
+    world.add(boundary.clone());
+    world.add(Rc::new(ConstantMedium::with_colour(
+        boundary.clone(),
+        0.2,
+        Colour::new(0.2, 0.4, 0.9),
+    )));
+    let boundary = Rc::new(Sphere::new(
+        Point3::zero(),
+        Point3::zero(),
+        5000.0,
+        Rc::new(Dielectric::new(1.5)),
+    ));
+    world.add(Rc::new(ConstantMedium::with_colour(
+        boundary.clone(),
+        0.0001,
+        Colour::one(),
+    )));
+
+    let emat = Rc::new(Lambertian::new(Rc::new(ImageTexture::new(
+        RtwImage::new("earthmap.jpg").unwrap(),
+    ))));
+    world.add(Rc::new(Sphere::new(
+        Point3::new(400.0, 200.0, 400.0),
+        Point3::new(400.0, 200.0, 400.0),
+        100.0,
+        emat,
+    )));
+    let pertext = Rc::new(NoiseTexture::<256>::new(0.2));
+    world.add(Rc::new(Sphere::new(
+        Point3::new(220.0, 280.0, 300.0),
+        Point3::new(220.0, 280.0, 300.0),
+        80.0,
+        Rc::new(Lambertian::new(pertext)),
+    )));
+
+    let mut boxes2 = HittableList::empty();
+    let white = Rc::new(Lambertian::from_colour(Colour::new(0.73, 0.73, 0.73)));
+    let ns = 1000;
+    for _ in 0..ns {
+        let pt = Point3::random_range(0.0, 165.0);
+        boxes2.add(Rc::new(Sphere::new(pt, pt, 10.0, white.clone())));
+    }
+
+    world.add(Rc::new(Translate::new(
+        Rc::new(RotateY::new(Rc::new(BVHNode::from_list(boxes2)), 15.0)),
+        Vec3::new(-100.0, 270.0, 395.0),
+    )));
+
+    let aspect_ratio: f64 = 1.0;
+    let background = Colour::zero();
+
+    let vfov: f64 = 40.0;
+    let look_from = Point3::new(478.0, 278.0, -600.0);
     let look_at = Point3::new(278.0, 278.0, 0.0);
     let vup = Vec3::new(0.0, 1.0, 0.0);
 
